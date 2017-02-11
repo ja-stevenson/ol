b62000235fa7f016b796d1d0f735c7835f906431
Jimmy Stevenson <jimmyastevenson@gmail.com>
2017-02-11 17:57:22 -0600

    small styling update

--- src/index.js
+++ src/index.js
@@ -93,7 +93,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="foo">
+      <div>
         <h2>OL Business Details</h2>
         {this.renderBusinessListOrDetails()}
       </div>
